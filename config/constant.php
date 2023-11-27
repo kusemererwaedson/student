ea075b72b69349125e1feb7fac765fca37d354ed
kusemererwaedson <kusemererwaedson2000@gmail.com>
2023-11-27 03:19:39 -0800

    hey

--- config/constant.php
+++ config/constant.php
@@ -1,16 +1,16 @@
 <?php
 // when offline
-session_start();
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'student_db');
-
-
-// // when online
 // session_start();
 // define('DB_HOST', 'localhost');
-// define('DB_USER', 'hakacdmf_edson');
-// define('DB_PASS', 'edson2000');
-// define('DB_NAME', 'hakacdmf_student');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'student_db');
+
+
+// when online
+session_start();
+define('DB_HOST', 'localhost');
+define('DB_USER', 'hakacdmf_edson');
+define('DB_PASS', 'edson2000');
+define('DB_NAME', 'hakacdmf_student');
 ?>
